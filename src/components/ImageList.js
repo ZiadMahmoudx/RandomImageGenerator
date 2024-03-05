078a9a599eb8f6325d7a31d6701ad9644ad70760
Ziad Mahmoud <ziadmahmoudx98@gmail.com>
2024-03-05 04:18:11 +0200

    last

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -10,7 +10,7 @@ const ImageList = () => {
     return <ImageShow key={image.id} image={image} />;
   });
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 justify-items-center	  gap-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 justify-items-center	items-baseline	  gap-4">
       {renderImages}
     </div>
   );
